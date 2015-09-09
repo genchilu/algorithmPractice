f9b851cc6271ad045fc6d3610d255be9f693787c
Genchi <genchilu@gmail.com>
2015-09-09 23:03:16 +0800

    Add goroutine at HW1, test performance

--- countingInversions/countingInversions.go
+++ countingInversions/countingInversions.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
 )
 
 func readInputToSlice(filename string) []int {
@@ -61,10 +62,26 @@ func countingInversions(slice []int) (int, []int) {
 		return 0, slice
 	} else {
 		mid := length / 2
-		countleft, sortedL := countingInversions(slice[0:mid])
-		countright, sortedR := countingInversions(slice[mid:length])
-		countMerge, sorted := countingMerge(sortedL, sortedR)
+		var countleft, countright, countMerge int
+		var sortedL, sortedR, sorted []int
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			countleft, sortedL = countingInversions(slice[0:mid])
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			countright, sortedR = countingInversions(slice[mid:length])
+		}()
+		wg.Wait()
+		countMerge, sorted = countingMerge(sortedL, sortedR)
 		return countleft + countright + countMerge, sorted
+		//		countleft, sortedL := countingInversions(slice[0:mid])
+		//		countright, sortedR := countingInversions(slice[mid:length])
+		//		countMerge, sorted := countingMerge(sortedL, sortedR)
+		//		return countleft + countright + countMerge, sorted
 	}
 }
 
