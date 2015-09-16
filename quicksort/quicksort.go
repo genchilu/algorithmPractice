d7a8fc3c0090c9353de64904ae46304de44b9411
Genchi <genchilu@gmail.com>
2015-09-17 00:36:46 +0800

    Fix over stack bug,

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -37,8 +37,10 @@ func swap(slice []int, i, j int) {
 	slice[j] = tmp
 }
 
-func quicksort(slice []int, start, end int) int {
-	if (end - start) == 0 {
+func quicksort(slice []int, start, end int, pivotType string) int {
+	//fmt.Println("start ", slice)
+	//fmt.Printf("start %d, end %d\n", start, end)
+	if (end - start) <= 0 {
 		return 0
 	} else if (end - start) == 1 {
 		if slice[start] > slice[end] {
@@ -46,6 +48,23 @@ func quicksort(slice []int, start, end int) int {
 		}
 		return 1
 	} else {
+		var pivotPos int
+		if pivotType == "first" {
+			pivotPos = start
+		} else if pivotType == "end" {
+			pivotPos = end
+		} else if pivotType == "mid" {
+			if (end-start+1)%2 == 0 {
+				pivotPos = ((end - start + 1) / 2) - 1 + start
+			} else {
+				pivotPos = (end-start)/2 + start
+			}
+		}
+
+		if pivotPos != start {
+			swap(slice, start, pivotPos)
+		}
+
 		i := start + 1
 		for j := (start + 1); j < (end + 1); j++ {
 			if slice[j] < slice[start] {
@@ -54,16 +73,24 @@ func quicksort(slice []int, start, end int) int {
 			}
 		}
 		swap(slice, (i - 1), start)
-		leftCount := quicksort(slice, start, (i - 2))
-		rightCount := quicksort(slice, i, end)
+		//fmt.Println("after ", slice)
+		leftCount := quicksort(slice, start, (i - 2), pivotType)
+		rightCount := quicksort(slice, i, end, pivotType)
 		return end - start + leftCount + rightCount
 	}
 }
 
 func main() {
 	slice := readInputToSlice("input")
-	//slice := []int{6, 1, 4, 2, 3, 7}
-	count := quicksort(slice, 0, len(slice)-1)
-	//fmt.Println(slice)
-	fmt.Println(count)
+	//slice := []int{6, 10, 1, 11, 9, 4, 2, 12, 8, 3, 13, 7}
+	count1 := quicksort(slice, 0, len(slice)-1, "start")
+	fmt.Println(count1)
+
+	//	slice = readInputToSlice("input")
+	//	count2 := quicksort(slice, 0, len(slice)-1, "end")
+	//	fmt.Println(count2)
+	//
+	//	slice = readInputToSlice("input")
+	//	count := quicksort(slice, 0, len(slice)-1, "mid")
+	//	fmt.Println(count)
 }
