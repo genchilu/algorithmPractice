38f5f4880b6f7309a2a7c52fc0462637a81a9dd1
GenchiLu <genchilu@gmail.com>
2020-05-14 02:51:58 +0800

    Add kata practice: golang + quicksort

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -1,118 +1,33 @@
-package main
+package quicksort
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-)
+//import "fmt"
 
-func readInputToSlice(filename string) []int {
-	var slice []int
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
+func Sort(input []uint){
+	if input != nil {
+		quick(input, 0, len(input)-1)
 	}
-	defer file.Close()
+} 
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		x, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			log.Fatal(err)
-		}
-		slice = append(slice, x)
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	return slice
-}
-
-func swap(slice []int, i, j int) {
-	tmp := slice[i]
-	slice[i] = slice[j]
-	slice[j] = tmp
-}
-
-func quicksort(slice []int, start, end int, pivotType string) int {
-	//fmt.Println("start ", slice)
-	//fmt.Printf("start %d, end %d\n", start, end)
-	if (end - start) <= 0 {
-		return 0
-	} else if (end - start) == 1 {
-		if slice[start] > slice[end] {
-			swap(slice, start, end)
-		}
-		return 1
-	} else {
-		var pivotPos int
-		if pivotType == "first" {
-			pivotPos = start
-		} else if pivotType == "end" {
-			pivotPos = end
-		} else if pivotType == "mid" {
-			var midPos int
-			if (end-start+1)%2 == 0 {
-				midPos = ((end - start + 1) / 2) - 1 + start
-			} else {
-				midPos = (end-start)/2 + start
-			}
-			if slice[start] > slice[midPos] && slice[start] > slice[end] {
-				if slice[midPos] > slice[end] {
-					pivotPos = midPos
-				} else {
-					pivotPos = end
-				}
-			} else if slice[midPos] > slice[start] && slice[midPos] > slice[end] {
-				if slice[start] > slice[end] {
-					pivotPos = start
-				} else {
-					pivotPos = end
-				}
-			} else {
-				if slice[start] > slice[midPos] {
-					pivotPos = start
-				} else {
-					pivotPos = midPos
-				}
+func quick(input []uint, begin int, end int) {
+	//fmt.Printf("before: %v, begin: %d, end: %d\n", input, begin, end);
+	if end > begin {
+		pivotIdx := begin
+		for i:= begin+1; i<=end; i++ {
+			if input[i] < input[begin] {
+				pivotIdx++
+				swap(input, pivotIdx, i)
 			}
 		}
 
-		if pivotPos != start {
-			swap(slice, start, pivotPos)
-		}
-
-		i := start + 1
-		for j := (start + 1); j < (end + 1); j++ {
-			if slice[j] < slice[start] {
-				swap(slice, i, j)
-				i++
-			}
-		}
-		swap(slice, (i - 1), start)
-		//fmt.Println("after ", slice)
-		leftCount := quicksort(slice, start, (i - 2), pivotType)
-		rightCount := quicksort(slice, i, end, pivotType)
-		comparCount := end - start
-		return comparCount + leftCount + rightCount
+		swap(input, begin, pivotIdx)
+		//fmt.Printf("after: %v, begin: %d, end: %d, pivotIdx: %d\n", input, begin, end, pivotIdx);
+		quick(input, begin, pivotIdx)
+		quick(input, pivotIdx+1, end)
 	}
 }
 
-func main() {
-	slice := readInputToSlice("input")
-	//slice := []int{6, 10, 1, 11, 9, 4, 2, 12, 8, 3, 13, 7}
-	count1 := quicksort(slice, 0, len(slice)-1, "first")
-	fmt.Println(count1)
-
-	slice = readInputToSlice("input")
-	count2 := quicksort(slice, 0, len(slice)-1, "end")
-	fmt.Println(count2)
-
-	slice = readInputToSlice("input")
-	count := quicksort(slice, 0, len(slice)-1, "mid")
-	//fmt.Println(slice)
-	fmt.Println(count)
+func swap(input []uint, a, b int) {
+	tmp := input[a]
+	input[a] = input[b]
+	input[b] = tmp
 }
---
+++ quicksort/quicksort_test.go
@@ -0,0 +1,34 @@
+package quicksort
+
+
+import (
+	"."
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSort(t *testing.T) {
+	testCases := []struct{
+		input []uint
+		expect []uint
+	}{
+		{nil, nil},
+		{[]uint{}, []uint{}},
+		{[]uint{1}, []uint{1}},
+		{[]uint{1,2}, []uint{1,2}},
+		{[]uint{2,1}, []uint{1,2}},
+		{[]uint{1,1}, []uint{1,1}},
+		{[]uint{1,2,3,4,5}, []uint{1,2,3,4,5}},
+		{[]uint{5,4,3,2,1}, []uint{1,2,3,4,5}},
+		{[]uint{7,5,3,3,9,6,7}, []uint{3,3,5,6,7,7,9}},
+		{[]uint{23, 42, 32, 64, 12, 4}, []uint{4, 12, 23, 32, 42, 64}},
+		{[]uint{2,1,2,2,2,2,2}, []uint{1,2,2,2,2,2,2}},
+		{[]uint{10,12,9,13,8,14,7}, []uint{7,8,9,10,12,13,14}},
+	}
+
+	for _, testCase := range testCases {
+		quicksort.Sort(testCase.input)
+
+		assert.Equal(t, testCase.expect, testCase.input);
+	}
+}
