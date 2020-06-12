c5b00cd8bf785e024d98e9eb7a1d99b22f944f7f
GenchiLu <genchilu@gmail.com>
2020-06-13 00:23:58 +0800

    Kata practice: quicksort in golang

--- quicksort/golang/quicksort.go
+++ quicksort/golang/quicksort.go
@@ -2,32 +2,39 @@ package quicksort
 
 //import "fmt"
 
-func Sort(input []uint){
-	if input != nil {
-		quick(input, 0, len(input)-1)
+func Sort(input []int){
+	if input != nil && len(input) >0 {
+		quick(input, 0, len(input))
 	}
 } 
 
-func quick(input []uint, begin int, end int) {
-	//fmt.Printf("before: %v, begin: %d, end: %d\n", input, begin, end);
-	if end > begin {
-		pivotIdx := begin
-		for i:= begin+1; i<=end; i++ {
+func quick(input []int, begin int, end int) {
+	if (end - begin) > 1 {
+		finalPivotIdx := begin
+
+		// 這邊可以自由更換選 pivot index 的方法
+		curPivotIdx := begin
+
+		// 強制把 pivot 放到 index begin
+		swap(input, curPivotIdx, begin)
+
+		for i:=(begin+1);i<end;i++ {
 			if input[i] < input[begin] {
-				pivotIdx++
-				swap(input, pivotIdx, i)
+				swap(input, i, finalPivotIdx+1)
+				finalPivotIdx++
 			}
 		}
+		swap(input, begin, finalPivotIdx)
 
-		swap(input, begin, pivotIdx)
-		//fmt.Printf("after: %v, begin: %d, end: %d, pivotIdx: %d\n", input, begin, end, pivotIdx);
-		quick(input, begin, pivotIdx)
-		quick(input, pivotIdx+1, end)
+		quick(input, begin, finalPivotIdx)
+		quick(input, finalPivotIdx+1, end)
 	}
 }
 
-func swap(input []uint, a, b int) {
-	tmp := input[a]
-	input[a] = input[b]
-	input[b] = tmp
+func swap(input []int, fromIdx int, toIdx int) {
+	if fromIdx != toIdx {
+		tmp := input[fromIdx]
+		input[fromIdx] = input[toIdx]
+		input[toIdx] = tmp
+	}
 }
