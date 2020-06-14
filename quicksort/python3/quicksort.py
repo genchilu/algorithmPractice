60a1166f535cc620a7c5384074b9635ec089f783
GenchiLu <genchilu@gmail.com>
2020-06-14 10:06:45 +0800

    Kata practice: quicksort + python3

--- quicksort/python3/quicksort.py
+++ quicksort/python3/quicksort.py
@@ -1,21 +1,27 @@
 def sort(input):
     if input is not None:
-        _quicksort(input, 0, len(input))
+        _quick_sort(input, 0, len(input))
 
-def _quicksort(input, begin, end):
-    if end > begin:
-        pivot = begin
-        for i in range(begin, end):
-            if input[i]<input[begin]:
-                pivot=pivot+1
-                _swap(input, i, pivot)
-        
-        _swap(input, begin, pivot)
-        _quicksort(input, begin, pivot)
-        _quicksort(input, pivot+1, end)
+def _quick_sort(input, begin, end):
+    if (end-begin) > 1:
+        pivot_idx = begin
 
-def _swap(input, a, b):
-    tmp=input[a]
-    input[a]=input[b]
-    input[b]=tmp
+        _swap(input, pivot_idx, begin)
+
+        final_pivot_idx = begin
+        for i in range(begin+1, end):
+            if input[i] < input[pivot_idx]:
+                final_pivot_idx = final_pivot_idx+1
+                _swap(input, i, final_pivot_idx)
+
+        _swap(input, begin, final_pivot_idx)
+
+        _quick_sort(input, begin, final_pivot_idx)
+        _quick_sort(input, final_pivot_idx+1, end)
+
+def _swap(input, from_idx, to_idx):
+    if(from_idx != to_idx):
+        tmp = input[from_idx]
+        input[from_idx]= input[to_idx]
+        input[to_idx] = tmp
         
