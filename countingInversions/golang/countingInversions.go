ec5c085895fd82c9c0d634f302e9374cf5e02c5f
GenchiLu <genchilu@gmail.com>
2020-06-15 22:45:14 +0800

    Kata practice: counting inversions + golang

--- countingInversions/golang/countingInversions.go
+++ countingInversions/golang/countingInversions.go
@@ -3,39 +3,46 @@ package countingInversion
 //import "fmt"
 
 func CountInversions(input []uint) int {
-	if(input == nil || len(input) <= 1) {
+
+	if (input == nil) {
 		return 0
 	}
 
-	count, _ := countWithMergeSort(input)
+	count, _ := counting(input)
 	return count
+
 }
 
-func countWithMergeSort(input []uint) (int, []uint) {
-	if len(input) == 1 {
+func counting(input []uint) (int, []uint){
+	if (len(input) <=1) {
 		return 0, input
-	} else {
-		lCount, lSlice := countWithMergeSort(input[0: len(input)/2])
-		rCount, rSlice := countWithMergeSort(input[len(input)/2: len(input)])
-
-		i:=0
-		j:=0
-		count := 0
-		var sortedSlice []uint
-		for i<len(lSlice) || j<len(rSlice){
-			if i == len(lSlice) {
-				sortedSlice = append(sortedSlice, rSlice[j])
-				j++
-			} else if j == len(rSlice) || lSlice[i] <= rSlice[j] {
-				sortedSlice = append(sortedSlice, lSlice[i])
-				i++
-			} else {
-				sortedSlice = append(sortedSlice, rSlice[j])
-				j++
-				count += len(lSlice) -i
-			}
-		}
-		//fmt.Printf("input: %v, l: %v, r: %v, lcount: %d, rcount: %d, count: %d\n", input, lSlice, rSlice, lCount, rCount, count)
-		return (count + lCount + rCount), sortedSlice
 	}
+
+	mid := len(input)/2
+	lcount, linput := counting(input[0:mid])
+	rcount, rinput := counting(input[mid:])
+
+	count :=0
+	mergeResult := []uint{}
+	lidx := 0
+	ridx := 0
+
+	for lidx<len(linput) || ridx<len(rinput) {
+		if (lidx == len(linput)) {
+			mergeResult = append(mergeResult, rinput[ridx:]...)
+			ridx = len(rinput)
+		} else if (ridx == len(rinput)) {
+			mergeResult = append(mergeResult, linput[lidx:]...)
+			lidx = len(linput)
+		} else if (linput[lidx] <= rinput[ridx]) {
+			mergeResult = append(mergeResult, linput[lidx])
+			lidx++
+		} else {
+			mergeResult = append(mergeResult, rinput[ridx])
+			ridx++
+			count+= len(linput) - lidx
+		}
+	}
+
+	return (count+lcount+rcount),  mergeResult
 }
