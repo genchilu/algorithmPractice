1be30dc1a9a97682511ea364ccb4e95369b1d96b
GenchiLu <genchilu@gmail.com>
2020-06-26 09:51:15 +0800

    kata practice: quicksort+golang

--- quicksort/golang/quicksort.go
+++ quicksort/golang/quicksort.go
@@ -3,38 +3,35 @@ package quicksort
 //import "fmt"
 
 func Sort(input []int){
-	if input != nil && len(input) >0 {
+	if (input != nil || len(input)> 0) {
 		quick(input, 0, len(input))
 	}
-} 
+}
 
-func quick(input []int, begin int, end int) {
-	if (end - begin) > 1 {
-		finalPivotIdx := begin
+func quick(input []int, begin, end int) {
+	if ((end-begin) > 1 ) {
+		pivotIdx := begin;
 
-		// 這邊可以自由更換選 pivot index 的方法
-		curPivotIdx := begin
+		swap(input, pivotIdx, begin)
+		pivotIdx = begin
 
-		// 強制把 pivot 放到 index begin
-		swap(input, curPivotIdx, begin)
-
-		for i:=(begin+1);i<end;i++ {
-			if input[i] < input[begin] {
-				swap(input, i, finalPivotIdx+1)
-				finalPivotIdx++
+		for i:=(begin+1);i<end;i++{
+			if(input[i] < input[begin]) {
+				pivotIdx++
+				swap(input, pivotIdx, i)
 			}
 		}
-		swap(input, begin, finalPivotIdx)
+		swap(input, pivotIdx, begin)
 
-		quick(input, begin, finalPivotIdx)
-		quick(input, finalPivotIdx+1, end)
+		quick(input, begin, pivotIdx)
+		quick(input, pivotIdx+1, end)
 	}
 }
 
-func swap(input []int, fromIdx int, toIdx int) {
-	if fromIdx != toIdx {
-		tmp := input[fromIdx]
-		input[fromIdx] = input[toIdx]
-		input[toIdx] = tmp
+func swap(input []int, from, to int) {
+	if (from != to) {
+		tmp := input[from]
+		input[from] = input[to]
+		input[to] = tmp
 	}
-}
+} 
