b6381a54904c63765890636c21e6b0bc681c3ef9
GenchiLu <genchilu@gmail.com>
2020-06-29 10:08:38 +0800

    kata practice: count inversion+golang

--- countingInversions/golang/countingInversions.go
+++ countingInversions/golang/countingInversions.go
@@ -3,46 +3,41 @@ package countingInversion
 //import "fmt"
 
 func CountInversions(input []uint) int {
-
-	if (input == nil) {
+	if (input == nil || len(input) <2) {
 		return 0
 	}
-
-	count, _ := counting(input)
+	_, count:=count(input)
 	return count
-
 }
 
-func counting(input []uint) (int, []uint){
-	if (len(input) <=1) {
-		return 0, input
+func count(input []uint) ([]uint, int) {
+	if (len(input) < 2) {
+		return input, 0
 	}
-
 	mid := len(input)/2
-	lcount, linput := counting(input[0:mid])
-	rcount, rinput := counting(input[mid:])
+	linput, lcount := count(input[0:mid])
+	rinput, rcount := count(input[mid:])
 
 	count :=0
-	mergeResult := []uint{}
-	lidx := 0
-	ridx := 0
-
-	for lidx<len(linput) || ridx<len(rinput) {
+	lidx :=0
+	ridx :=0
+	sortedInput := []uint{}
+	for lidx <len(linput) || ridx < len(rinput) {
 		if (lidx == len(linput)) {
-			mergeResult = append(mergeResult, rinput[ridx:]...)
+			sortedInput = append(sortedInput, rinput[ridx:]...)
 			ridx = len(rinput)
 		} else if (ridx == len(rinput)) {
-			mergeResult = append(mergeResult, linput[lidx:]...)
+			sortedInput = append(sortedInput, linput[lidx:]...)
 			lidx = len(linput)
 		} else if (linput[lidx] <= rinput[ridx]) {
-			mergeResult = append(mergeResult, linput[lidx])
+			sortedInput = append(sortedInput, linput[lidx])
 			lidx++
 		} else {
-			mergeResult = append(mergeResult, rinput[ridx])
+			sortedInput = append(sortedInput, rinput[ridx])
 			ridx++
-			count+= len(linput) - lidx
+			count+=len(linput) - lidx
 		}
 	}
-
-	return (count+lcount+rcount),  mergeResult
+	return sortedInput, count+lcount+rcount
 }
+
