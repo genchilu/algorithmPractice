aeddb1e40654239954a23ad7179d89dc7c8edfc6
GenchiLu <genchilu@gmail.com>
2020-07-15 10:16:39 +0800

    kata practice: quicksort + java

--- quicksort/java/src/main/java/QuickSort.java
+++ quicksort/java/src/main/java/QuickSort.java
@@ -1,36 +1,36 @@
 public class QuickSort {
     public static void sort(int[] input) {
-        if (input!=null) {
-            quicksort(input, 0, input.length);
+        if(input != null && input.length > 1) {
+            quickSort(input, 0, input.length);
         }
     }
 
-    private static void quicksort(int[] input, int begin, int end) {
-        if((end-begin) > 1) {
+    private static void quickSort(int[] input, int begin, int end) {
+        if ((end-begin) > 1) {
             int pivotIdx = begin;
 
             swap(input, pivotIdx, begin);
+
             pivotIdx = begin;
 
-            for(int i=begin+1;i<end;i++){
-                if(input[i] < input[begin]){
+            for (int i = (begin+1);i<end;i++) {
+                if(input[i] < input[begin]) {
                     pivotIdx++;
-                    swap(input, i, pivotIdx);
+                    swap(input, pivotIdx, i);
                 }
             }
 
-            swap(input, begin, pivotIdx);
-            quicksort(input, begin, pivotIdx);
-            quicksort(input, pivotIdx+1, end);
+            swap(input, pivotIdx, begin);
+
+            quickSort(input, begin, pivotIdx);
+            quickSort(input, pivotIdx+1, end);
         }
     }
-
     private static void swap(int[] input, int from, int to) {
-        if(from != to) {
+        if (from!=to) {
             int tmp = input[from];
             input[from] = input[to];
             input[to] = tmp;
         }
     }
-
 }
