85fbfe424fcfb958dbb435ccccd953c2b5b8ca02
GenchiLu <genchilu@gmail.com>
2020-07-21 09:47:10 +0800

    kata practice: count inversions + python3

--- countingInversions/python3/countingInversions.py
+++ countingInversions/python3/countingInversions.py
@@ -1,36 +1,38 @@
 def countInversions(input):
-    if input == None:
+    if input == None or len(input) <=1:
         return 0
-    count, sort_inpiut = _count(input)
+
+    [sorted_array, count] = counting(input)
 
     return count
 
-def _count(input):
-    if (len(input)<2):
-        return 0, input
-    mid = len(input)//2
-    lcount, linput = _count(input[0:mid])
-    rcount, rinput = _count(input[mid:])
 
-    lidx = 0
-    ridx = 0
+def counting(input):
+    if len(input) <= 1:
+        return input, 0
+    
+    mid = len(input)//2
+    [linput, lcount] = counting(input[0:mid])
+    [rinput, rcount] = counting(input[mid:])
+
     count = 0
-    sorted_input = []
+    lidx = 0
+    ridx =0
+    sort_array = []
 
     while lidx < len(linput) or ridx < len(rinput):
-        if (lidx == len(linput)):
-            sorted_input.extend(rinput[ridx:])
+        if lidx == len(linput):
+            sort_array.extend(rinput[ridx:])
             ridx = len(rinput)
-        elif (ridx == len(rinput)):
-            sorted_input.extend(linput[lidx:])
+        elif ridx == len(rinput):
+            sort_array.extend(linput[lidx:])
             lidx = len(linput)
-        elif (linput[lidx] <= rinput[ridx]):
-            sorted_input.append(linput[lidx])
-            lidx += 1
+        elif linput[lidx] <= rinput[ridx]:
+            sort_array.append(linput[lidx])
+            lidx = lidx+1
         else:
-            sorted_input.append(rinput[ridx])
-            ridx += 1
-            count += len(linput) - lidx
-        
-    return (count+lcount+rcount), sorted_input
-
+            sort_array.append(rinput[ridx])
+            ridx = ridx+1
+            count = count + len(linput) - lidx
+    
+    return sort_array, count+lcount+rcount
