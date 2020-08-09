4612c0c60de7bb9c8e67120ab8fd046a976b190d
GenchiLu <genchilu@gmail.com>
2020-08-09 20:29:04 +0800

    kata practice: kosaraju scc + python3

--- kosaraju/python3/kosaraju.py
+++ kosaraju/python3/kosaraju.py
@@ -1,4 +1,7 @@
 def do_kosaraju_scc(edges):
+    if edges == None or len(edges) == 0:
+        return {}
+
     g = {}
     for edge in edges:
         from_v = edge[0]
@@ -7,54 +10,57 @@ def do_kosaraju_scc(edges):
             g[from_v] = []
         if to_v not in g:
             g[to_v] = []
-        g[from_v].append(to_v) 
+        g[from_v].append(to_v)
 
     is_vistited1 = {}
     finish_stack = []
     for v in g:
-        _dfs_round1(g, v, is_vistited1, finish_stack)
+        _dfs1(g, v, is_vistited1, finish_stack)
 
-    rev_g = _reverse_g(g)
+    rev_g = _rever_g(g)
 
-
-    componments = {}
     is_vistited2 = {}
-    while(len(finish_stack) > 0):
-        v = finish_stack.pop()
+    componments = {}
+    while len(finish_stack) > 0:
+        v = finish_stack[-1]
+        finish_stack = finish_stack[0:-1]
         componment = []
-        _dfs_round2(rev_g, v, is_vistited2, componment)
+        _dfs2(rev_g, v, is_vistited2, componment)
+
         if len(componment) > 0:
             componment.sort()
             componments[componment[0]] = componment
-
+    
     return componments
+        
 
-def _dfs_round1(g, cur_v, is_vistited, finish_stack):
-    if cur_v not in is_vistited:
-        is_vistited[cur_v] = True
-        for v in g[cur_v]:
-            _dfs_round1(g, v, is_vistited, finish_stack)
-        finish_stack.append(cur_v)
 
-def _reverse_g(g):
+
+
+def _dfs1(g, v, is_vistited, finish_stack):
+    if v not in is_vistited:
+        is_vistited[v] = True
+        for to_v in g[v]:
+            _dfs1(g, to_v, is_vistited, finish_stack)
+        finish_stack.append(v)
+
+def _rever_g(g):
     rev_g = {}
     for from_v in g:
         if from_v not in rev_g:
             rev_g[from_v] = []
-            
         for to_v in g[from_v]:
             if to_v not in rev_g:
                 rev_g[to_v] = []
-            rev_g[to_v].append(from_v)
+            rev_g[to_v].append(from_v) 
     return rev_g
-
-def _dfs_round2(g, cur_v, is_vistited, componment):
-    if cur_v not in is_vistited:
-        is_vistited[cur_v] = True
-        for v in g[cur_v]:
-            _dfs_round2(g, v, is_vistited, componment)
-        componment.append(cur_v)
-
+        
+def _dfs2(g, v, is_vistited, componment):
+    if v not in is_vistited:
+        is_vistited[v] = True
+        for to_v in g[v]:
+            _dfs2(g, to_v, is_vistited, componment)
+        componment.append(v)
 
 
 
