d5ba9fb251d27e374cd176c2fcc35401f9c8d1f3
GenchiLu <genchilu@gmail.com>
2020-09-11 10:31:29 +0800

    kata practice: quicksort + golang

--- quicksort/golang/quicksort.go
+++ quicksort/golang/quicksort.go
@@ -3,36 +3,36 @@ package quicksort
 //import "fmt"
 
 func Sort(input []int){
-	if (input!=nil && len(input)>1) {
-		quicksort(input, 0, len(input))
+	if input != nil && len(input) !=0 {
+		doQuockSort(input, 0, len(input))
 	}
 }
 
-func quicksort(input []int, begin, end int) {
-	if ((end-begin) > 1) {
-		pivotIdx := begin
+func doQuockSort(input []int, begin, end int) {
 
+	if(end-begin) > 0 {
+		pivotIdx := begin
 		swap(input, pivotIdx, begin)
+
 		pivotIdx = begin
 
-		for i:=(begin+1);i<end;i++ {
-			if(input[i] < input[begin]) {
-				pivotIdx++
+		for i:=pivotIdx+1;i<end;i++ {
+			if (input[i] < input[begin]) {
 				swap(input, i, pivotIdx)
 			}
 		}
-
 		swap(input, begin, pivotIdx)
 
-		quicksort(input, begin, pivotIdx)
-		quicksort(input, pivotIdx+1, end)
+		doQuockSort(input, begin, pivotIdx)
+		doQuockSort(input, pivotIdx+1, end)
 	}
 }
 
 func swap(input []int, from, to int) {
-	if (from != to) {
-		tmp := input[from]
-		input[from] = input[to]
-		input[to] = tmp
-	}
+	tmp := input[from]
+	input[from] = input[to]
+	input[to] = tmp
 }
+
+
+
