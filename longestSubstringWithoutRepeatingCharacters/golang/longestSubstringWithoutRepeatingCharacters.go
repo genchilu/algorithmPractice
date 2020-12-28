a6e8b7f36962fe9fc3498314e4502ec34ab57e06
GenchiLu <genchilu@gmail.com>
2020-12-28 12:17:22 +0800

    improve Longest Substring Without Repeating Characters

--- longestSubstringWithoutRepeatingCharacters/golang/longestSubstringWithoutRepeatingCharacters.go
+++ longestSubstringWithoutRepeatingCharacters/golang/longestSubstringWithoutRepeatingCharacters.go
@@ -1,23 +1,24 @@
 package longestSubstringWithoutRepeatingCharacters
 
 func lengthOfLongestSubstring(s string) int {
-	m := make(map[byte]int)
+	m := make([]int, 127)
+	for i, _ := range m {
+		m[i] = -1
+	}
 	b := 0
 
 	max := 0
 	for i := 0; i < len(s); i++ {
-		if _, ok := m[s[i]]; ok {
-			if m[s[i]] >= b {
-				l := i - b
-				if l > max {
-					//fmt.Printf("111  %d, %d, %d, %d\n", max, l, i, b)
-					max = l
-				}
-				b = m[s[i]] + 1
+		if m[int(s[i])] > -1 && m[s[i]] >= b {
+			l := i - b
+			if l > max {
+				//fmt.Printf("111  %d, %d, %d, %d\n", max, l, i, b)
+				max = l
 			}
+			b = m[s[i]] + 1
 			//fmt.Printf("333  %d, %d\n", i, b)
 		}
-		m[s[i]] = i
+		m[int(s[i])] = i
 	}
 
 	l := len(s) - b
