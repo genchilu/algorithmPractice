d1d0195aa3f93cc6deb775f492489b72d338f9bd
Genchi Lu <genchi.lu@TW168020131M.local>
2021-04-17 01:18:56 +0800

    leetcode practice: subsets

---
+++ subsets/golang/subsets.go
@@ -0,0 +1,15 @@
+package subsets
+
+func subsets(nums []int) [][]int {
+
+	subsets := [][]int{{}}
+
+	for _, num := range nums {
+		for _, subset := range subsets {
+			newSubset := append([]int{}, subset...)
+			newSubset = append(newSubset, num)
+			subsets = append(subsets, newSubset)
+		}
+	}
+	return subsets
+}
