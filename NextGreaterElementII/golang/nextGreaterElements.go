d0b4251b0fb074552cacdb50e181085beebc9a5d
Genchi Lu <genchi.lu@TW168020131M.local>
2021-07-10 00:04:02 +0800

    leetcode practice: improve Next Greater Element II

--- NextGreaterElementII/golang/nextGreaterElements.go
+++ NextGreaterElementII/golang/nextGreaterElements.go
@@ -1,25 +1,20 @@
 package nextGreaterElements
 
-type INum struct {
-	idx int
-	val int
-}
-
 func nextGreaterElements(nums []int) []int {
 
 	result := make([]int, len(nums))
 	for i := range result {
 		result[i] = -1
 	}
-	stack := []INum{}
+	stack := []int{}
 	for i := 0; i < 2; i++ {
 		for j := len(nums) - 1; j >= 0; j-- {
 
 			for len(stack) > 0 {
 				l := len(stack) - 1
-				if stack[l].val > nums[j] {
+				if nums[stack[l]] > nums[j] {
 					if result[j] == -1 {
-						result[j] = stack[l].val
+						result[j] = nums[stack[l]]
 					}
 					break
 				} else {
@@ -27,7 +22,7 @@ func nextGreaterElements(nums []int) []int {
 				}
 			}
 
-			stack = append(stack, INum{idx: j, val: nums[j]})
+			stack = append(stack, j)
 		}
 	}
 	return result
