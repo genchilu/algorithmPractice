b0a9d3a6bfc5260fa2cfd1f846a7f7d0406569a4
Genchi Lu <genchi.lu@TW168020131M.local>
2021-07-09 15:52:41 +0800

    leetcode practice: 4sumII

--- 4SumII/golang/4sumII.go
+++ 4SumII/golang/4sumII.go
@@ -2,25 +2,18 @@ package fourSumCount
 
 func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
 
-	m := make(map[int]int)
+	m := make(map[int]int, len(nums1)*len(nums2))
 
 	for _, i := range nums1 {
 		for _, j := range nums2 {
-			s := i + j
-			if _, ok := m[s]; !ok {
-				m[s] = 0
-			}
-			m[s] = m[s] + 1
+			m[i+j]++
 		}
 	}
 
 	c := 0
 	for _, i := range nums3 {
 		for _, j := range nums4 {
-			s := 0 - (i + j)
-			if _, ok := m[s]; ok {
-				c += m[s]
-			}
+			c += m[0-i-j]
 		}
 	}
 	return c
