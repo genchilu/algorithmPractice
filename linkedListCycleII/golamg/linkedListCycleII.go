4de68f107a53a650fca146d4e4bb853fcc46b6a5
Genchi Lu <genchi.lu@TW168020131M.local>
2021-08-07 15:31:28 +0800

    leetcode practice: Linked List Cycle II

---
+++ linkedListCycleII/golamg/linkedListCycleII.go
@@ -0,0 +1,40 @@
+package linkedListCycleII
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	f, s := head, head
+	for {
+		if f.Next == nil || f.Next.Next == nil {
+			return nil
+		}
+		f = f.Next.Next
+		s = s.Next
+
+		if s == f {
+			break
+		}
+	}
+
+	if s == head {
+		return s
+	}
+	s = head
+
+	for {
+		f = f.Next
+		s = s.Next
+		if s == f {
+			break
+		}
+	}
+
+	return s
+}
