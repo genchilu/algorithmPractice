7a7dffc0051a7e7cc5de5358716f289ae4f365b8
Genchi Lu <genchi.lu@TW168020131M.local>
2021-08-10 00:32:59 +0800

    improve Maximum Number of Events That Can Be Attended with heap

--- maximumNumberOfEventsThatCanBeAttended/golang/maximumNumberOfEventsThatCanBeAttended.go
+++ maximumNumberOfEventsThatCanBeAttended/golang/maximumNumberOfEventsThatCanBeAttended.go
@@ -1,8 +1,27 @@
 package maximumNumberOfEventsThatCanBeAttended
 
 import (
+	"container/heap"
 	"sort"
 )
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 func maxEvents(events [][]int) int {
 
@@ -11,36 +30,30 @@ func maxEvents(events [][]int) int {
 	})
 
 	idx := 0
-	end := []int{}
+	h := &IntHeap{}
+	heap.Init(h)
 	count := 0
 	for i := 0; i <= 100000; i++ {
 		for idx < len(events) {
 			if events[idx][0] == i {
 				e := events[idx][1]
-				insert := sort.SearchInts(end, e)
-				end = append(end, 0)
-				copy(end[insert+1:], end[insert:])
-				end[insert] = e
+				heap.Push(h, e)
 			} else if events[idx][0] > i {
 				break
 			}
 			idx++
 		}
 
-		j := 0
-		for ; j < len(end); j++ {
-			if end[j] >= i {
-				break
-			}
+		for h.Len() > 0 && (*h)[0] < i {
+			heap.Pop(h)
 		}
-		end = end[j:]
 
-		if len(end) > 0 {
+		if h.Len() > 0 {
 			count++
-			end = end[1:]
+			heap.Pop(h)
 		}
 
-		if idx >= len(events) && len(end) == 0 {
+		if idx >= len(events) && h.Len() == 0 {
 			break
 		}
 	}
