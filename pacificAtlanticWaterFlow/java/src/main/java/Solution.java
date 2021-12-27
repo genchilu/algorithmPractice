2f06a8e97ddc9c33c486faee254624b373d00cf4
Genchi Lu <genchi.lu@TW168020131M.local>
2021-12-27 09:21:52 +0800

    leetcode practice: Pacific Atlantic Water Flow

--- pacificAtlanticWaterFlow/java/src/main/java/Solution.java
+++ pacificAtlanticWaterFlow/java/src/main/java/Solution.java
@@ -1,31 +1,32 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Flow;
 
 class Solution {
-    class FlowRes{
-        boolean toAtlantic;
-        boolean toPacific;
 
-        public FlowRes(boolean toAtlantic, boolean toPacific) {
-            this.toAtlantic = toAtlantic;
-            this.toPacific = toPacific;
-        }
-    }
 
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> res = new ArrayList<>();
-        boolean[][] seen = new boolean[heights.length][heights[0].length];
+        boolean[][] pacificFlow = new boolean[heights.length][heights[0].length];
+        for(int i =0;i<heights.length;i++) {
+            checkFlow(heights,i,0,pacificFlow);
+        }
+        for(int j =0;j<heights[0].length;j++) {
+            checkFlow(heights,0,j,pacificFlow);
+        }
 
-        //0 ori / 1 up/2 down/3 left/4 right
-        FlowRes[][][] cache = new FlowRes[heights.length][heights[0].length][5];
+        boolean[][] atlanticFlow = new boolean[heights.length][heights[0].length];
+        for(int i =0;i<heights.length;i++) {
+            checkFlow(heights,i, heights[0].length-1,atlanticFlow);
+        }
+        for(int j =0;j<heights[0].length;j++) {
+            checkFlow(heights,heights.length-1,j,atlanticFlow);
+        }
 
-        for(int i = 0;i<heights.length;i++){
+        for(int i =0;i<heights.length;i++){
             for(int j=0;j<heights[0].length;j++){
-                FlowRes flowRes = checkFlow(heights, i, j, seen, 0, cache);
-                if(flowRes.toAtlantic && flowRes.toPacific) {
+                if(pacificFlow[i][j] && atlanticFlow[i][j]) {
                     res.add(Arrays.asList(i,j));
                 }
             }
@@ -34,62 +35,32 @@ class Solution {
         return res;
     }
 
-    public FlowRes checkFlow(int[][] heights, int i, int j, boolean[][] seen, int direction, FlowRes[][][] cache) {
-        if(cache[i][j][direction] != null) {
-            return cache[i][j][direction];
-        }
+    public void checkFlow(int[][] heights,int i,int j, boolean[][] flowed) {
+        flowed[i][j] = true;
 
-        FlowRes res = new FlowRes(false, false);
-        if(i==0 || j ==0) {
-            res.toPacific = true;
-        }
-        if(i==heights.length-1 || j == heights[0].length-1) {
-            res.toAtlantic = true;
-        }
-
-        seen[i][j] = true;
         if(i>0) {
-            if(!seen[i-1][j] && heights[i-1][j] <= heights[i][j]) {
-                FlowRes nextRes = checkFlow(heights, i-1,j,seen, 1, cache);
-                updateFlowRes(nextRes, res);
+            if(!flowed[i-1][j] && heights[i-1][j]>=heights[i][j]) {
+                checkFlow(heights, i-1, j, flowed);
             }
         }
 
         if(i<heights.length-1) {
-            if(!seen[i+1][j] && heights[i+1][j]<=heights[i][j]) {
-                FlowRes nextRes = checkFlow(heights, i+1,j,seen, 2, cache);
-                updateFlowRes(nextRes, res);
+            if(!flowed[i+1][j] && heights[i+1][j]>=heights[i][j]) {
+                checkFlow(heights, i+1, j, flowed);
             }
         }
 
         if(j>0) {
-            if(!seen[i][j-1] && heights[i][j-1]<=heights[i][j]) {
-                FlowRes nextRes = checkFlow(heights, i,j-1,seen, 3, cache);
-                updateFlowRes(nextRes, res);
+            if(!flowed[i][j-1] && heights[i][j-1]>=heights[i][j]) {
+                checkFlow(heights, i, j-1, flowed);
             }
         }
 
         if(j<heights[0].length-1) {
-            if(!seen[i][j+1] && heights[i][j+1]<=heights[i][j]) {
-                FlowRes nextRes = checkFlow(heights, i,j+1,seen, 4, cache);
-                updateFlowRes(nextRes, res);
+            if(!flowed[i][j+1] && heights[i][j+1]>=heights[i][j]) {
+                checkFlow(heights, i, j+1, flowed);
             }
         }
-        seen[i][j] = false;
 
-        cache[i][j][direction] = res;
-        return res;
-    }
-
-    public FlowRes updateFlowRes(FlowRes next, FlowRes cur) {
-        if(next.toPacific) {
-            cur.toPacific = true;
-        }
-
-        if(next.toAtlantic) {
-            cur.toAtlantic = true;
-        }
-
-        return cur;
     }
 }
